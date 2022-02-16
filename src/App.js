85aaecb519d0917e8c7b51d3d73f72680c62ead1
albertlai431 <albertlai631@outlook.com>
2022-02-16 14:52:50 -0500

    Add routing and auth support

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Flex, VStack, Button, Box, HStack, Link } from "@chakra-ui/react";
+import { Routes, Route, Link as RouterLink } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
+import EventsListing from "./pages/EventsListingPage";
+import EventPage from "./pages/EventPage";
+import AuthPage from "./pages/AuthPage";
 
 function App() {
+  const [auth, setAuth] = useState(false);
+  const location = useLocation();
+
+  const logOut = () => {
+    setAuth(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box px={16} py={8} w="full">
+      <VStack w="full">
+        {location.pathname === "/login" ? (
+          <Flex justifyContent="flex-start" w="full">
+            <Link as={RouterLink} to="/" fontSize="xl">
+              Home
+            </Link>
+          </Flex>
+        ) : (
+          <HStack justifyContent="space-between" w="full">
+            <Link as={RouterLink} to="/" fontSize="xl">
+              Home
+            </Link>
+            {auth ? (
+              <Button onClick={logOut}>Log out</Button>
+            ) : (
+              <Button
+                as={RouterLink}
+                to={"/login"}
+                state={{ previousPath: location.pathname }}
+              >
+                Log in
+              </Button>
+            )}
+          </HStack>
+        )}
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <EventsListing key={auth} loggedIn={auth} setAuth={setAuth} />
+            }
+          />
+          <Route
+            path="/event/:id"
+            element={<EventPage key={auth} loggedIn={auth} />}
+          />
+          <Route path="login" element={<AuthPage setAuth={setAuth} />} />
+        </Routes>
+      </VStack>
+    </Box>
   );
 }
 
